b9c4a712dfeee313c64ce6f1e8c503abbe60f151
Charlie Lindviken <charlie.lindviken@bontouch.com>
2016-07-14 17:45:44 +0200

    added templates

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -17,4 +17,74 @@ app.controller('MainCtrl',['$scope', function ($scope) {
 	$scope.removeFromList = function(index) {
 		$scope.template.list.splice(index,1);
 	}
+
+
+
+	$scope.infoTemplate = {
+		title: '',
+		subtitle: '',
+		images: '',
+		groups: []
+	};
+
+	$scope.groupTemplate = {
+		title: '',
+		items: []
+	};
+
+	$scope.personItemTemplate = {
+		type: '',
+		name: '',
+		title: '',
+		email: '',
+		number: ''
+	};
+
+	$scope.textItemTemplate = {
+		type: '',
+		content: ''
+	};
+
+	$scope.imageItemTemplate = {
+		type: '',
+		url: ''
+	};
+
+
+	$scope.newsTemplate = {
+		title: '',
+		time: '',
+		content: '',
+		author: '',
+		image: '',
+		externalUrl: ''
+	};
+
+	$scope.eventTimeline = {
+		type: '',
+		title: '',
+		time: '',
+		description: '',
+		endTime: '',
+		allDay: 'false',
+		location: {},
+		tag: ''
+	};
+
+	$scope.locationTemplate = {
+		name: '',
+		description: '',
+		lat: '',
+		lng: ''
+	};
+
+	$scope.taskTimeline = {
+		type: '',
+		title: '',
+		time: '',
+		allDay: 'false',
+		description: '',
+		remind: 'false',
+		tag: ''
+	};
 }]);

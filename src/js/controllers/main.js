e5718f03c205944d1f2f10eb478704e01d2babab
Shayan Effati <shayan@effati.com>
2016-07-19 15:28:16 +0200

    full functionality

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -3,11 +3,16 @@
 /*globals _ */
 app.controller('MainCtrl',['$scope', function ($scope) {
 
-	$scope.modules = [{name: 'nyheter', type:'news', list: []}];
+	$scope.modules = [];
 	$scope.moduleName = '';
 	$scope.moduleType = '';
 
+	$scope.timelineType = '';
+	$scope.infoItemType = '';
+
 	$scope.moduleTypes = ['news', 'info', 'timeline'];
+	$scope.timelineTypes = ['task', 'event'];
+	$scope.infoItemTypes = ['person', 'text', 'image'];
 
 
 	$scope.template = {
@@ -19,33 +24,42 @@ app.controller('MainCtrl',['$scope', function ($scope) {
 	$scope.module = {};
 	$scope.list = [];
 
-	$scope.itemTitle = '';
 	$scope.itemTime = '';
 	$scope.itemContent = '';
 	$scope.itemAuthor = '';
 	$scope.itemImage = '';
 	$scope.itemExternalUrl = '';
 
-	$scope.addTaskToModule = function(index) {
-		$scope.modules[index].list.push(_.cloneDeep($scope.taskTimeline))
-	};
-	
-	$scope.addEventToModule = function(index) {
-		$scope.modules[index].list.push(_.cloneDeep($scope.eventTimeline))
-	};
+	$scope.itemTitle = '';
+	$scope.itemTime = '';
+	$scope.itemAllDay = '';
+	$scope.itemDescription = '';
+	$scope.itemRemind = '';
+	$scope.itemTag = '';
+
+	$scope.infoGroups = [];
+	var infoGroup = {};
+	infoGroup.title = '';
+	infoGroup.list = [];
+
+	$scope.itemName = '';
+	$scope.itemTitle = '';
+	$scope.itemEmail = '';
+	$scope.itemNumber = '';
+	$scope.itemContent = '';
+	$scope.itemUrl = '';
 
 	$scope.removeItem = function(moduleIndex, itemIndex) {
-		if(!itemIndex) {
-			if(confirm('Do you want to remove ' + $scope.modules[moduleIndex].name)) {
+		if(typeof itemIndex === 'undefined') {
+			if(confirm('Do you want to remove ' + $scope.modules[moduleIndex].name + '?')) {
 				$scope.modules.splice(moduleIndex, 1);
 			}
-			
 			return;
 		}
-
-		$scope.modules[moduleIndex].list.splice(itemIndex, 1);
+		if(confirm('Do you want to remove ' + $scope.modules[moduleIndex].list[itemIndex].title + '?')) {
+			$scope.modules[moduleIndex].list.splice(itemIndex, 1);
+		}
 	}
-
 
 	$scope.addNewsItem = function(index, itemTitle, itemTime, itemContent, itemAuthor, itemImage, itemExternalUrl) {
 		var clone = _.cloneDeep($scope.newsTemplate);
@@ -65,21 +79,61 @@ app.controller('MainCtrl',['$scope', function ($scope) {
 		$scope.itemAuthor = '';
 		$scope.itemImage = '';
 		$scope.itemExternalUrl = '';
+	};
 
+	$scope.addTaskItem = function(index, itemTitle, itemTime, itemAllDay, itemDescription, itemRemind, itemTag) {
+		var clone = _.cloneDeep($scope.taskTimeline);
 
+		clone.type = 'task';
+		clone.title = itemTitle;
+		clone.time = itemTime;
+		clone.allDay = itemAllDay;
+		clone.description = itemDescription;
+		clone.remind = itemRemind;
+		clone.tag = itemTag;
+
+		$scope.modules[index].list.push(clone);
+
+		$scope.itemTitle = '';
+		$scope.itemTime = '';
+		$scope.itemAllDay = '';
+		$scope.itemDescription = '';
+		$scope.itemRemind = '';
+		$scope.itemTag = '';
+	};
+
+	$scope.addEventItem = function(index, itemTitle, itemTime, itemAllDay, itemDescription, itemEndTime, itemLocation, itemTag) {
+		var clone = _.cloneDeep($scope.eventTimeline);
+
+		clone.type = 'event';
+		clone.title = itemTitle;
+		clone.time = itemTime;
+		clone.allDay = itemAllDay;
+		clone.description = itemDescription;
+		clone.endTime = itemEndTime;
+		clone.location = itemLocation;
+		clone.tag = itemTag;
+
+		$scope.modules[index].list.push(clone);
+
+		$scope.itemTitle = '';
+		$scope.itemTime = '';
+		$scope.itemAllDay = '';
+		$scope.itemDescription = '';
+		$scope.itemEndTime = '';
+		$scope.itemLocation = '';
+		$scope.itemTag = '';
 	};
 
 	$scope.addItemToModule = function(index, moduleType) {
-		console.log(index)
-		console.log(moduleType)
 		switch(moduleType) {
 			case 'news':
 			$scope.modules[index].list.push(_.cloneDeep($scope.newsTemplate));
 			break;
-			case 'info': 
+			case 'info':
 			$scope.modules[index].list.push(_.cloneDeep($scope.infoTemplate));
 			break;
-			case 'timeline': 
+			case 'timeline':
 			$scope.modules[index].list.push(_.cloneDeep($scope.timelineTemplate));
 			break;
 			default:
@@ -99,12 +153,78 @@ app.controller('MainCtrl',['$scope', function ($scope) {
 		$scope.template.list.splice(index,1);
 	}
 
+	$scope.removeGroupItem = function(moduleIndex, groupIndex, itemIndex) {
+		$scope.modules[moduleIndex].list[groupIndex].groups.splice(itemIndex, 1);
+	}
 
+	$scope.addInfoItem = function(index, itemTitle, itemSubtitle, itemImage) {
+		var clone = _.cloneDeep($scope.infoTemplate);
+
+		clone.title = itemTitle;
+		clone.subtitle = itemSubtitle;
+		clone.image = itemImage;
+
+		$scope.modules[index].list.push(clone);
+
+		$scope.itemTitle = '';
+		$scope.itemSubtitle = '';
+		$scope.itemImage = '';
+	}
+
+	$scope.addGroup = function(moduleIndex, itemIndex, groupTitle) {
+		var infoGroup = {};
+		infoGroup.title = groupTitle;
+		infoGroup.list = [];
+		$scope.modules[moduleIndex].list[itemIndex].groups.push(infoGroup);
+		/*$scope.infoGroups.push(infoGroup);*/
+	}
+
+	$scope.addPersonItem = function(moduleIndex, infoIndex, groupIndex, itemName, itemTitle, itemEmail, itemNumber) {
+		console.log($scope.modules[moduleIndex]);
+		console.log($scope.modules[moduleIndex].list[infoIndex]);
+		console.log($scope.modules[moduleIndex].list[infoIndex].groups[groupIndex]);
+		var clone = _.cloneDeep($scope.personItemTemplate);
+
+		clone.type = 'person';
+		clone.name = itemName;
+		clone.title = itemTitle;
+		clone.email = itemEmail;
+		clone.number = itemNumber;
+
+		$scope.modules[moduleIndex].list[infoIndex].groups[groupIndex].list.push(clone);
+
+		$scope.itemName = '';
+		$scope.itemTitle = '';
+		$scope.itemEmail = '';
+		$scope.itemNumber = '';
+	}
+
+	$scope.addTextItem = function(moduleIndex, infoIndex, groupIndex, itemContent) {
+		var clone = _.cloneDeep($scope.textItemTemplate);
+
+		clone.type = 'text';
+		clone.content = itemContent;
+
+		$scope.modules[moduleIndex].list[infoIndex].groups[groupIndex].list.push(clone);
+
+		$scope.itemContent = '';
+	}
+
+	$scope.addImageItem = function(moduleIndex, infoIndex, groupIndex, itemUrl) {
+		var clone = _.cloneDeep($scope.imageItemTemplate);
+
+		clone.type = 'image';
+		clone.url = itemUrl;
+
+		$scope.modules[moduleIndex].list[infoIndex].groups[groupIndex].list.push(clone);
+
+		$scope.itemUrl = '';
+	}
 
 	$scope.infoTemplate = {
 		title: '',
 		subtitle: '',
-		images: '',
+		image: '',
 		groups: []
 	};
 
@@ -149,7 +269,7 @@ app.controller('MainCtrl',['$scope', function ($scope) {
 		type: '',
 		title: '',
 		time: '',
-		allDay: 'false',
+		allDay: false,
 		description: '',
 		endTime: '',
 		location: {},
@@ -167,12 +287,11 @@ app.controller('MainCtrl',['$scope', function ($scope) {
 		type: '',
 		title: '',
 		time: '',
-		allDay: 'false',
+		allDay: false,
 		description: '',
-		remind: 'false',
+		remind: false,
 		tag: ''
 	};
-
 
 	$scope.$watch('modules', function(newValue) {
 		var internal = _.cloneDeep(newValue);
@@ -185,8 +304,5 @@ app.controller('MainCtrl',['$scope', function ($scope) {
 		});
 		$scope.nAppenModules = internal;
 	}, true);
-
-
-
 
 }]);
